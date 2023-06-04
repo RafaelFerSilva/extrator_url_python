f8d6f2109807fc07f67d55bb5f119a102a4fbfd2
Rafael Silva <todosomtodavoz@gmail.com>
2023-06-04 09:43:03 -0300

    Utilizando o len para retornar o tamanho de uma string

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 
 print(url)
 
@@ -6,8 +6,16 @@ indice_interrogacao = url.find('?')
 
 url_base = url[:indice_interrogacao]
 url_parametros = url[indice_interrogacao+1:]
+parametro_busca = 'moedaDestino'
+indice_parametro = url_parametros.find(parametro_busca)
+
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+
 
 print(url_base)
 print(url_parametros)
+print(indice_parametro)
+print(valor)
 
 
