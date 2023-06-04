98448b73fbea2902ca8a110f366d67917b0f808a
Rafael Silva <todosomtodavoz@gmail.com>
2023-06-04 09:56:54 -0300

    Validando o & comercial entre os parametros para trabalhar com parâmetros míltiplos

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
 
 print(url)
 
+# Separa base e parâmetros
 indice_interrogacao = url.find('?')
 
 url_base = url[:indice_interrogacao]
 url_parametros = url[indice_interrogacao+1:]
-parametro_busca = 'moedaDestino'
+
+# Busca valores de um parâmetro
+parametro_busca = 'moedaOrigem'
 indice_parametro = url_parametros.find(parametro_busca)
-
 indice_valor = indice_parametro + len(parametro_busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find('&', indice_valor)
 
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 
 print(url_base)
 print(url_parametros)
